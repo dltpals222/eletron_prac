c1f84f5bbbb6f22187628695f60863e735dd6a81
blahblah <leesemin222@gmail.com>
2024-11-12 17:38:55 +0900

    폴더 트리 변경

--- Electron_prac/main.js
+++ Electron_prac/main.js
@@ -1,32 +1,38 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const axios = require('axios');
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
 
-  win.loadFile('index.html');
+    win.loadFile('index.html');
+
+    checkForUpdates();
 }
 
-ipcMain.on('get-version', (event) => {
-  event.reply('version', app.getVersion());
-});
+app.whenReady().then(createWindow);
 
-app.on('ready', createWindow);
+async function checkForUpdates() {
+    try {
+        const response = await axios.get('http://localhost:3000/latest-version');
+        const latestVersion = response.data.version;
+        const currentVersion = app.getVersion();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+        if (currentVersion !== latestVersion) {
+            dialog.showMessageBox({
+                type: 'info',
+                buttons: ['OK'],
+                title: 'Update Available',
+                message: `A new version (${latestVersion}) is available. Please update your app.`,
+            });
+        }
+    } catch (error) {
+        console.error('Failed to check for updates:', error);
+    }
+}
